8b8030cde731fd8899b77774f0765ce79d78788c
Derble <49699980+Derble@users.noreply.github.com>
2019-06-01 16:51:59 -0700

    Create Boolean Logic program

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);//AND
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(true || true);//OR
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(true == true);//EQUAL
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            Console.WriteLine(true != true);//NOT EQUAL
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            Console.WriteLine(true ^ true);//XOR will evaluate to true but only if one not both are true
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.WriteLine(true && true && true && false);
+            Console.WriteLine(true && true && true || false);
+
+            Console.ReadLine();
+        }
+    }
+}
