034320fe29ea3e30387961b08496795a957ac220
Derble <49699980+Derble@users.noreply.github.com>
2019-06-03 14:04:30 -0700

    Create Package Express application with  branching and logic

---
+++ PackageExpressApplication/PackageExpressApplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpressApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            double packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight <= 50)
+            {
+                Console.WriteLine("Please enter the package width:");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+
+                double dimensions = packageWidth + packageHeight + packageLength;
+
+                if (dimensions <= 50)
+                {
+
+                    double estimatedTotal = dimensions * packageWeight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + estimatedTotal);
+                }
+                else
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
