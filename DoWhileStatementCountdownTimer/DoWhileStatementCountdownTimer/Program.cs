96b395b2d706e9494fcc66b97e6fe74268127c21
Derble <49699980+Derble@users.noreply.github.com>
2019-06-03 23:03:04 -0700

    Create do while program countdown timer page 94

---
+++ DoWhileStatementCountdownTimer/DoWhileStatementCountdownTimer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileStatementCountdownTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do while
+            //press enter to count down
+            int x = 10;
+            do
+            {
+                Console.WriteLine(x);
+                Console.ReadLine();
+                x--;
+                if (x == 0)
+                {
+                    Console.WriteLine("Blast off!!!");
+                }
+            }
+            while (x > 0);
+            Console.ReadLine();
+            
+        }
+    }
+}
