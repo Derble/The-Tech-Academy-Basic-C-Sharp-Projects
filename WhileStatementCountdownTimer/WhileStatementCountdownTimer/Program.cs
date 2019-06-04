d90f0354177af7b0adc16b90308e09175e444201
Derble <49699980+Derble@users.noreply.github.com>
2019-06-03 23:01:25 -0700

    Create while program countdown timer page 94

---
+++ WhileStatementCountdownTimer/WhileStatementCountdownTimer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileStatementCountdownTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            int x = 10;
+            while (x > 0)
+            {
+                Console.WriteLine(x);
+                x--;
+                Console.ReadLine();
+            }
+            if (x == 0)
+            {
+                Console.WriteLine("Blast off!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
