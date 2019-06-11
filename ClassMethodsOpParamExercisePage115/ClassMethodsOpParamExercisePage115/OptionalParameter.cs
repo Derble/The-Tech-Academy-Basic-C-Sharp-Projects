45611fa51166c1774892a0bfb7e2d0b8be0a0feb
Derble <49699980+Derble@users.noreply.github.com>
2019-06-11 12:48:05 -0700

    Create optional parameters exercise for page 115

---
+++ ClassMethodsOpParamExercisePage115/ClassMethodsOpParamExercisePage115/OptionalParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsOpParamExercisePage115
+{
+    class OptionalParameter
+    {
+        public int someMoreMath(int a, int b = 12)
+        {
+            int c = a * b;
+            return c;
+        }
+    }
+}
---
+++ ClassMethodsOpParamExercisePage115/ClassMethodsOpParamExercisePage115/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsOpParamExercisePage115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Still working on this
+            try
+            {
+                Console.WriteLine("Hello, please enter an integer:");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("You may enter a second number if you'd like to:");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                
+                OptionalParameter n = new OptionalParameter();
+
+                int mathIsDone = n.someMoreMath(numOne, numTwo);
+                Console.WriteLine(mathIsDone);
+            }
+            catch (FormatException ex)
+            {
+                int numTwo = 12;
+
+            }
+            catch (Exception ex)
+            {
+                int numTwo = 12;
+            }
+            
+
+            
+            Console.ReadLine();
+        }
+    }
+}
