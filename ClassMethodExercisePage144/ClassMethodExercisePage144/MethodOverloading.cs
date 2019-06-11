2b18acdb2271be66a4e2a252c61ea57dc32fdaab
Derble <49699980+Derble@users.noreply.github.com>
2019-06-11 11:57:49 -0700

    Create program with method overloading

---
+++ ClassMethodExercisePage144/ClassMethodExercisePage144/MethodOverloading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercisePage144
+{
+    class MethodOverloading
+    {
+        public int someMath(int a)
+        {
+            int addFive = a + 5;
+            return addFive;
+        }
+
+        public int someMath(double b)
+        {
+            int multiplyFive = Convert.ToInt32(b * 5);
+            return multiplyFive;
+        }
+
+        public int someMath(string c)
+        {
+            int subtractFive = Convert.ToInt32(c) - 5;
+            return subtractFive;
+        }
+
+    }
+}
---
+++ ClassMethodExercisePage144/ClassMethodExercisePage144/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercisePage144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOverloading n = new MethodOverloading();
+
+            int numOne = 8;
+            int firstMethodAnswer = n.someMath(numOne);
+            Console.WriteLine(firstMethodAnswer);
+
+            MethodOverloading m = new MethodOverloading();
+
+            double numTwo = 12.56;
+            int secondMethodAnswer = m.someMath(numTwo);
+            Console.WriteLine(secondMethodAnswer);
+
+            MethodOverloading x = new MethodOverloading();
+
+            string numThree = "9";
+            int thirdMethodAnswer = x.someMath(numThree);
+            Console.WriteLine(thirdMethodAnswer);
+
+
+            //This code also works but does not instantiate the class at each step like the directions asked.
+            /*
+            MethodOverloading n = new MethodOverloading();
+
+            int numOne = 8;
+            int firstMethodAnswer = n.someMath(numOne);
+            Console.WriteLine(firstMethodAnswer);
+
+            double numTwo = 12.56;
+            int secondMethodAnswer = n.someMath(numTwo);
+            Console.WriteLine(secondMethodAnswer);
+
+            string numThree = "9";
+            int thirdMethodAnswer = n.someMath(numThree);
+            Console.WriteLine(thirdMethodAnswer);
+            */
+
+
+            Console.ReadLine();
+        }
+    }
+}
