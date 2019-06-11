2503d22f5e3f116d54e95d9fe8403cd9db677d07
Derble <49699980+Derble@users.noreply.github.com>
2019-06-10 23:55:55 -0700

    Create program using void method, class, and parameters

---
+++ ExercisePage113Methods/ExercisePage113Methods/MathOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisePage113Methods
+{
+    class MathOperation
+    {
+        
+        
+
+        public void OperationAndDisplay(int a, int b)
+        {
+            a = a * 4;
+            //Console.WriteLine(a);//used for checking that everything is functioning
+            Console.WriteLine(b);
+            
+        }
+    }
+}
---
+++ ExercisePage113Methods/ExercisePage113Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisePage113Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 7;
+            int d = 5;
+            MathOperation i = new MathOperation();
+            i.OperationAndDisplay(7, 5);//passing in the two numbers directly
+            i.OperationAndDisplay(c, d);//specifying parameters by name
+            Console.ReadLine();
+        }
+        
+    }
+}
