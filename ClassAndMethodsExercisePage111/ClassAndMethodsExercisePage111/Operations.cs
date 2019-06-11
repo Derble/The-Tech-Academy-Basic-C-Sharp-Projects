c90ef84375b7ebbb4bc3f14f95f85469d26a3a0d
Derble <49699980+Derble@users.noreply.github.com>
2019-06-10 23:03:17 -0700

    Create class and method exercise using operations for page 111

---
+++ ClassAndMethodsExercisePage111/ClassAndMethodsExercisePage111/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsExercisePage111
+{
+    class Operations
+    {
+        public int multipliedByTwo(int a)
+        {
+            int multiply = a * 2;
+            return multiply;
+        }
+
+        public int addSix(int b)
+        {
+            int add = b + 6;
+            return add;
+        }
+
+        public int totalAfterDivideByTwo(int c)
+        {
+            int divide = c / 2;
+            return divide;
+        }
+
+    }
+}
---
+++ ClassAndMethodsExercisePage111/ClassAndMethodsExercisePage111/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsExercisePage111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to work with?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Operations n = new Operations();
+
+            int two = n.multipliedByTwo(userInput);
+            int six = n.addSix(two);
+            int result = n.totalAfterDivideByTwo(six);
+
+            Console.WriteLine(userInput + " multiplied by two, added with six, then divided by two equals " + result);
+            Console.ReadLine();
+
+            //For testing
+            //Console.WriteLine(two);
+            //Console.WriteLine(six);
+            //Console.WriteLine(result);
+
+            //first code to make sure operations work
+            //public Operations(userInput)
+            //{
+            //    int multipliedByTwo = userInput * 2;
+            //    int addSix = multipliedByTwo + 6;
+            //    int totalAfterDivideByTwo = addSix / 2;
+            //    return totalAfterDivideByTwo;
+            //}
+
+        }
+
+
+
+    }
+}
