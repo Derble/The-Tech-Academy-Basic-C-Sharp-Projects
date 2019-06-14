7ca1de9747c910e28da02e85e38e0ccf270943cd
Derble <49699980+Derble@users.noreply.github.com>
2019-06-14 12:09:13 -0700

    Create interface exercise for page 126

---
+++ InterfaceExercisePage126/InterfaceExercisePage126/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercisePage126
+{
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I quit!");
+        }
+        //public override void SayName()
+        //{
+        //    base.SayName();
+        //}
+    }
+}
