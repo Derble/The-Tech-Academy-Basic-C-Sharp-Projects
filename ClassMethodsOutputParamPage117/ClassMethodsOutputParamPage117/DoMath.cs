4d91970530479d28692a5522e0fd1b5b737ae4cd
Derble <49699980+Derble@users.noreply.github.com>
2019-06-13 20:18:26 -0700

    Create program using void method, static class, output parameters, overload method

---
+++ ClassMethodsOutputParamPage117/ClassMethodsOutputParamPage117/DoMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsOutputParamPage117
+{
+    class DoMath
+    {
+        public void DivideByTwo(int a, out int b)
+        {
+            b = a / 2;
+
+        }
+        
+        /*//No longer needed
+        public void DivideByTwo(double a, out double b)
+        {
+            b = a / 2;
+
+        }
+        */
+
+        public void DivideByTwo(decimal a, out decimal b)
+        {
+            b = a / 2;
+
+        }
+
+        public static class Greeting
+        {
+            public static string GetGreeting()
+            {
+                return "Please enter an integer:";
+            }
+        }
+    }
+}
---
+++ ClassMethodsOutputParamPage117/ClassMethodsOutputParamPage117/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsOutputParamPage117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter an integer:");
+            Console.WriteLine(DoMath.Greeting.GetGreeting());
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            int result;
+            DoMath n = new DoMath();
+            n.DivideByTwo(numOne, out result);
+            Console.WriteLine(result);
+
+            /*//Created before decimal using double because of some conversion errors. Figured it out.
+            Console.WriteLine("Please enter a decimal:");
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+            double result2;
+            n.DivideByTwo(numTwo, out result2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Please enter a decimal:");
+            decimal numThree = Convert.ToDecimal(Console.ReadLine());
+            decimal result3;
+            n.DivideByTwo(numThree, out result3);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
