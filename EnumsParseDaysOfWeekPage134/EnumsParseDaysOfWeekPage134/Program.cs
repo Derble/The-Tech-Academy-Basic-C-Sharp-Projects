44f0a07d4134c2c6c24b7ff6123c05d0863d1ed4
Derble <49699980+Derble@users.noreply.github.com>
2019-06-18 16:57:25 -0700

    Create and parse enum for days of the week

---
+++ EnumsParseDaysOfWeekPage134/EnumsParseDaysOfWeekPage134/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsParseDaysOfWeekPage134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+                string day = Console.ReadLine();
+                DaysOfTheWeek dayOfTheWeek = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), day);//converts string into enum type
+                Console.WriteLine("You entered a day of the week, " + dayOfTheWeek + "!");
+                
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            catch (FormatException ex)//need to find exception for entering integer, this won't catch it
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
