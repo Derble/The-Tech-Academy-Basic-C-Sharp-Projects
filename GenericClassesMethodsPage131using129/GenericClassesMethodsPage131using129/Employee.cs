85455aec97133fda8c9648de9c0659cbf548f1b7
Derble <49699980+Derble@users.noreply.github.com>
2019-06-17 22:31:12 -0700

    Create generic class program using int and string lists.

---
+++ GenericClassesMethodsPage131using129/GenericClassesMethodsPage131using129/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassesMethodsPage131using129
+{
+    class Employee<T> : Person
+    {
+        public List<T> Things { get; set; }
+
+        public int Id { get; set; }
+
+        public void SayId()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + " ID #: " + Id);
+        }
+        
+
+    }
+}
---
+++ GenericClassesMethodsPage131using129/GenericClassesMethodsPage131using129/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassesMethodsPage131using129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.Things = new List<string>() { "Mae", "Gregg", "Angus", "Bea", "Germ" };
+            foreach (string n in employeeString.Things)
+            {
+                Console.WriteLine(n);
+            }
+
+            Employee<int> employeeInt = new Employee<int>();
+            employeeInt.Things = new List<int>() { 22, 21, 23, 23, 19 };
+            foreach (int m in employeeInt.Things)
+            {
+                Console.WriteLine(m);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
