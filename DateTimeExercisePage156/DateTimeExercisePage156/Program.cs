9daf35bc87c15748dbe0583e2b6479fa4a851673
Derble <49699980+Derble@users.noreply.github.com>
2019-06-20 22:43:34 -0700

    Create datetime exercise, add x hours to the current datetime

---
+++ DateTimeExercisePage156/DateTimeExercisePage156/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercisePage156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("It will be: " + DateTime.Now.AddHours(hours) + " in " + hours + " hour(s).");
+            Console.ReadLine();
+        }
+    }
+}
