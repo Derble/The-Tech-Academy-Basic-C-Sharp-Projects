98d39a8add88eee173d444be9936468cc7ffc353
Derble <49699980+Derble@users.noreply.github.com>
2019-06-20 21:08:21 -0700

    Create File I/O Exercise logging a number to a text file then reading it back to the user.

---
+++ FileIOExercisePage154/FileIOExercisePage154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOExercisePage154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string writeText = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Derry\Desktop\number_entered.txt", writeText);
+            string readText = File.ReadAllText(@"C:\Users\Derry\Desktop\number_entered.txt");
+            Console.WriteLine("The number from the text file is: {0}", readText);
+            Console.ReadLine();
+        }
+    }
+}
